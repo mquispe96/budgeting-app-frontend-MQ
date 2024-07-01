ef4b81a7feb1a05aa5ba81d9677b3e2fdef8f54b
mquispe96 <mquispe@pursuit.org>
2024-07-01 11:56:59 -0400

    Setup App browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,26 @@
+import {Route, Routes, Navigate} from 'react-router-dom';
+import NavBar from './Pages/NavBar';
+import ShowTransactions from './Pages/ShowTransactions';
+import ShowTransaction from './Pages/ShowTransaction';
+import NewTransaction from './Pages/NewTransaction';
+import EditTransaction from './Pages/EditTransaction';
+import LogIn from './Pages/LogIn';
+import SignUp from './Pages/SignUp';
 
+const App = () => {
+  // <Route index element={<ShowTransactions />} />
+  // <Route path="/transaction/:id" element={<ShowTransaction />} />
+  // <Route path="/transaction/new" element={<NewTransaction />} />
+  // <Route path="/transaction/edit/:id" element={<EditTransaction />} />
+  // <Route path="/login" element={<LogIn />} />
+  // <Route path="/signup" element={<SignUp />} />
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/transactions" replace/>} />
+      <Route path="/transactions" element={<NavBar />}>
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
